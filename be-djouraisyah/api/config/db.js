ef005ad1ec729903012074c079fd19cbda6cc91f
wahyu <wabredz1234@gmail.com>
2024-07-08 06:19:08 +0700

    konfigurasi

---
+++ be-djouraisyah/api/config/db.js
@@ -0,0 +1,22 @@
+// db.js
+import mariadb from "mariadb";
+import "dotenv/config";
+
+dotenv.config();
+
+export const pool = mariadb.createPool({
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+});
+
+pool
+  .getConnection()
+  .then(() => {
+    console.log("Berhasil terhubung ke basis data.");
+  })
+  .catch((err) => {
+    console.error("Gagal terhubung ke basis data:", err);
+  });
