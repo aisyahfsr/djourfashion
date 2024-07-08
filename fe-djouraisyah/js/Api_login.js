38380175ef1e8c8dcb8bb27cf0f771ae1a40ce63
wahyu <wabredz1234@gmail.com>
2024-07-08 07:06:57 +0700

    update logika js login

--- fe-djouraisyah/js/Api_login.js
+++ fe-djouraisyah/js/Api_login.js
@@ -1,37 +1,35 @@
-function postRegFormData(event) {
-    event.preventDefault();
+async function postRegFormData(event) {
+  event.preventDefault();
 
-    // Ambil data dari form
-    const form = event.target;
-    const username = form.querySelector('[name="username"]').value;
-    const password = form.querySelector('[name="password"]').value;
+  // Ambil data dari form
+  const form = event.target;
+  const username = form.querySelector('[name="username"]').value;
+  const password = form.querySelector('[name="password"]').value;
 
-    // Buat objek data
-    const data = {
-        user_name: username,
-        password: password
+  try {
+    const response = await fetch("http://localhost:3000/api/user/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ username, password }),
+    });
+
+    if (!response.ok) {
+      const errorMessage = await response.json();
+      throw new Error(errorMessage.error || "Gagal melakukan login.");
     }
 
-    fetch("https://djouraisyah-production.up.railway.app/login", {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify(data),
-        })
-        .then(function (response) {
-            if (response.ok) {
-                return response.json();
-            }
-            return Promise.reject(response);
-        })
-        .then(function (data) {
-            alert('Login Telah Berhasil');
-            localStorage.setItem("user", JSON.stringify(data.data))
-            window.location.replace("index.html")
-        }).catch(function (error) {
-            console.log(error);
-        });
+    const data = await response.json();
+    console.log(data); // Di sini Anda bisa mengelola respons dari server
+    alert("Login berhasil!");
+    window.location.href = "index.html";
+  } catch (error) {
+    console.error("Error:", error.message);
+    alert("Login gagal. Silakan coba lagi.");
+  }
 }
 
-document.querySelector('#loginForm').addEventListener('submit', postRegFormData);
+document
+  .querySelector("#loginForm")
+  .addEventListener("submit", postRegFormData);
