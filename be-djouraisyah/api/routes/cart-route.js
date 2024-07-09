7f398fc033ebf57c5e8d2dc97346dcbc41aed401
wahyu <wabredz1234@gmail.com>
2024-07-09 20:15:34 +0700

    meng update route untuk cart

--- be-djouraisyah/api/routes/cart-route.js
+++ be-djouraisyah/api/routes/cart-route.js
@@ -2,14 +2,14 @@ import express from "express";
 import {
   addToCart,
   deleteCart,
-  getCartByIdCustomer,
+  getCartByIdUser,
   updateCart,
 } from "../controllers/cart-controller.js";
 
 const router = express.Router();
 
 router.post("/add-to-cart", addToCart);
-router.get("/get-cart/:id", getCartByIdCustomer);
+router.get("/get-cart/:id", getCartByIdUser);
 router.put("/update-quantity/:id", updateCart);
 router.delete("/delete-item/:id", deleteCart);
 
