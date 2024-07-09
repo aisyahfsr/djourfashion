19243083dba70125ce3ebb763ee8a0201731b7ab
wahyu <wabredz1234@gmail.com>
2024-07-09 20:15:49 +0700

    menambahkan rute get sales by id

--- be-djouraisyah/api/routes/sale-route.js
+++ be-djouraisyah/api/routes/sale-route.js
@@ -1,8 +1,9 @@
 import express from "express";
-import { addSales } from "../controllers/sale-controller.js";
+import { addSales, getSalesByIdIUser } from "../controllers/sale-controller.js";
 
 const router = express.Router();
 
 router.post("/checkout", addSales);
+router.get("/get-sales/:id", getSalesByIdIUser);
 
 export default router;
